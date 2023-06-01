0e9da68b6a6e479e8df6dc7d231d2bead9e32417
Rohit Sinha <rohit123sinha456@gmail.com>
2023-06-01 06:12:45 -0400

    Event driven Image Chnages Happening

--- multiprocess.py
+++ multiprocess.py
@@ -3,19 +3,16 @@ from display import Display
 from PIL import Image,ImageTk
 import multiprocessing as mp
 import time
-
-def idk():
-    print("In idl")
-    time.sleep(5)
-    display.update()
+from rabbitmqclient import MessageBroker as MB
+import threading 
 
 if __name__ == "__main__":
     pilimage = Image.open("lion.png")
     display = Display()
-    p1 = mp.Process(target=display.show,args=(pilimage,))
-    p2 = mp.Process(target=idk)
-    p1.start()
-    p2.start()
-
+    messagebroker = MB(display.root)
+    display.show(pilimage)
+    th = threading.Thread(target=messagebroker.consume)
+    th.start()
+    display.run()
 
     
--- rabbitmqclient.py
+++ rabbitmqclient.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import pika,sys,os
-def callback(ch,method,properties,body):
-    print(type(body))
-    print(str(body) == "H")
-    print("[x] Recieved %r"%body)
 
-def main():
-    conn = pika.BlockingConnection(pika.ConnectionParameters(host='10.12.1.131'))
-    channel = conn.channel()
-    channel.queue_declare(queue="test")
-    channel.basic_consume(queue="test",on_message_callback=callback,auto_ack=True)
-    print("[x] Waiting for Message")
-    channel.start_consuming()
+class MessageBroker:
+    def __init__(self,eventloop):
+        self.host = '10.12.1.131'
+        self.eventloop = eventloop
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='test')
+
+    def callback(self,ch,method,properties,body):
+        print("[x] Recieved %r"%body)
+        if(self.eventloop is not None):
+            if(body == b'H'):
+                self.eventloop.event_generate('<<SIG>>',when='tail')
+
+    def consume(self):
+        self.channel.basic_consume(queue="test",on_message_callback=self.callback,auto_ack=True)
+        print("[x] Waiting for Message")
+        self.channel.start_consuming()
+
 
 if __name__=="__main__":
-    main()
+    mb = MessageBroker(None)
+    mb.consume()
